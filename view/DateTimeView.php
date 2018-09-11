b7186eef924bd7404ad200d9158e17aada907a27
David Sturesson <sturessondavid@gmail.com>
2018-09-11 13:53:01 +0200

    Adding date-time string

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -5,8 +5,8 @@ class DateTimeView {
 
 	public function show() {
 
-		$timeString = 'TODO, Write servertime here...';
+		  $timeString = date('l, \t\h\e jS \of F Y, \T\h\e \t\i\m\e \i\s h:i:s A');
 
-		return '<p>' . $timeString . '</p>';
+			return '<p>' . $timeString . '</p>';
 	}
 }
--- view/LoginView.php
+++ view/LoginView.php
@@ -10,7 +10,7 @@ class LoginView {
 	private static $keep = 'LoginView::KeepMeLoggedIn';
 	private static $messageId = 'LoginView::Message';
 
-	
+
 
 	/**
 	 * Create HTTP response
@@ -21,7 +21,7 @@ class LoginView {
 	 */
 	public function response() {
 		$message = '';
-		
+
 		$response = $this->generateLoginFormHTML($message);
 		//$response .= $this->generateLogoutButtonHTML($message);
 		return $response;
@@ -40,7 +40,7 @@ class LoginView {
 			</form>
 		';
 	}
-	
+
 	/**
 	* Generate HTML code on the output buffer for the logout button
 	* @param $message, String output message
@@ -48,11 +48,11 @@ class LoginView {
 	*/
 	private function generateLoginFormHTML($message) {
 		return '
-			<form method="post" > 
+			<form method="post" >
 				<fieldset>
 					<legend>Login - enter Username and password</legend>
 					<p id="' . self::$messageId . '">' . $message . '</p>
-					
+
 					<label for="' . self::$name . '">Username :</label>
 					<input type="text" id="' . self::$name . '" name="' . self::$name . '" value="" />
 
@@ -61,16 +61,16 @@ class LoginView {
 
 					<label for="' . self::$keep . '">Keep me logged in  :</label>
 					<input type="checkbox" id="' . self::$keep . '" name="' . self::$keep . '" />
-					
+
 					<input type="submit" name="' . self::$login . '" value="login" />
 				</fieldset>
 			</form>
 		';
 	}
-	
+
 	//CREATE GET-FUNCTIONS TO FETCH REQUEST VARIABLES
 	private function getRequestUserName() {
 		//RETURN REQUEST VARIABLE: USERNAME
 	}
-	
+
 }
