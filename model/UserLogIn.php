196f7b6e8ded78cbb5bc73b4e69cc11d5637c0df
David Sturesson <sturessondavid@gmail.com>
2018-09-18 10:56:09 +0200

    Adding log in functionality

--- model/UserLogIn.php
+++ model/UserLogIn.php
@@ -1,19 +1,22 @@
 <?php
 
+require_once('database.php');
+
 class UserLogIn {
 
   public function userLogsIn($username, $password) {
-    $server = "localhost";
-		$user = "test";
-		$pass = "test";
-		$database = "auth";
-		try {
-			$connection = new PDO("mysql:host=$server;dbname=$database;", $user, $pass);
-			echo 'connection!';
-		}
-		catch(PDOException $e) {
-			$message = $e->getMessage();
-			echo 'Connection failed';
+		$db = new Database();
+		$connection = $db->connectToDatabase();
+		$selection = $connection->prepare('SELECT id,name,password FROM users WHERE name = :name');
+		$selection->bindParam(':name', $username);
+		$selection->execute();
+		$match = $selection->fetch(PDO::FETCH_ASSOC);
+		var_dump($match);
+
+		if(count($match) > 0 && password_verify($password, $match['password'])) {
+			echo 'Logged in';
+		} else {
+			echo 'Not logged in';
 		}
   }
 }
--- model/UserRegister.php
+++ model/UserRegister.php
@@ -7,13 +7,12 @@ class UserRegister {
     $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
     $db = new Database();
     $sql = "INSERT INTO users (name, password) VALUES (:name, :password)";
-    $temp = $db->connectToDatabase();
-    $stmt = $temp->prepare($sql);
+    $connect = $db->connectToDatabase();
+    $stmt = $connect->prepare($sql);
     $stmt->bindParam(':name', $username);
     $stmt->bindParam(':password', $hashedPassword);
     try {
     $stmt->execute();
-    echo 'haha de gick';
     }
     catch(PDOException $e) {
       echo $e->getMessage();
